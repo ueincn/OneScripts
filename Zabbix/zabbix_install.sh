38da8e28d3a3cd9dd1ec75d78b10475e1db792d4
fufouaka <fufouaka@163.com>
2023-10-14 21:10:00 +0800

    ueincn

--- Zabbix/zabbix_install.sh
+++ Zabbix/zabbix_install.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+if [ $UID -eq 0 ]; then
+    Main
+else
+    echo ""
+    echo "[ $(tput setaf 1)!! Please use sudo permissions or switch root to run the script !! $(tput sgr0)]"
+    echo ""
+fi
