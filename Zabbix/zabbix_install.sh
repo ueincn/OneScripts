e2932038fe7abc910673500d05d23542eaec4123
fufouaka <fufouaka@163.com>
2023-10-14 21:12:57 +0800

    ueincn

--- Zabbix/zabbix_install.sh
+++ Zabbix/zabbix_install.sh
@@ -2,9 +2,7 @@
 
 
 #判断权限
-if [ $UID -eq 0 ]; then
-    Main
-else
+if [ $UID -gt 0 ]; then
     echo ""
     echo "[ $(tput setaf 1)!! Please use sudo permissions or switch root to run the script !! $(tput sgr0)]"
     echo ""
