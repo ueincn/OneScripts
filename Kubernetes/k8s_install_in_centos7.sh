007762079ab58fa4ddbc55800b4f2bc08b7af9b5
fufouaka <fufouaka@163.com>
2023-10-18 20:24:27 +0800

    ueincn

--- Kubernetes/k8s_install_in_centos7.sh
+++ Kubernetes/k8s_install_in_centos7.sh
@@ -6,3 +6,10 @@
 #   2023   Ueincn  Release
 #Platform
 #   CentOS 7.9.2009
+
+#判断权限
+if [ $UID -gt 0 ]; then
+    echo ""
+    echo "[ $(tput setaf 1)!! Please use sudo permissions or switch root to run the script !! $(tput sgr0)]"
+    echo ""
+fi
