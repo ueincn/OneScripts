b87c7e0eb273c442b06dd98af1e6338f66102f2e
fufouaka <fufouaka@163.com>
2023-10-20 22:58:20 +0800

    ueincn

--- Gitlab/gitlab_install.sh
+++ Gitlab/gitlab_install.sh
@@ -1,1 +1,8 @@
 #！/bin/bash
+
+#判断权限
+if [ $UID -gt 0 ]; then
+    echo ""
+    echo "[ $(tput setaf 1)!! Please use sudo permissions or switch root to run the script !! $(tput sgr0)]"
+    echo ""
+fi
